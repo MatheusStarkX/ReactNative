dd8f1a158258b9493f3bf341b8a72a859bf6347b
MatheusStarkX <matheusfelipemdeassis@gmail.com>
2024-02-09 01:36:09 -0300

    Aula 3 - NLW Expert Rocketseat

--- src/app/product/[id].tsx
+++ src/app/product/[id].tsx
@@ -1,6 +1,7 @@
 import { View, Image, Text } from "react-native"
+import { Feather } from "@expo/vector-icons"
+import { Redirect } from "expo-router"
 import { useLocalSearchParams, useNavigation } from "expo-router"
-import { Feather } from "@expo/vector-icons";
 
 import { useCartStore } from "@/stores/cart-store";
 
@@ -15,18 +16,30 @@ export default function Product(){
     const navigation = useNavigation()
     const {id} = useLocalSearchParams()
 
-    const product = PRODUCTS.filter((item) => item.id === id)[0]
+    const product = PRODUCTS.find((item) => item.id === id)
 
     function handleAddToCart() {
-        cartStore.add(product)
-        navigation.goBack()
+        if(product){
+            cartStore.add(product)
+            navigation.goBack()
+        }
+    }
+
+    if(!product){
+        return <Redirect href="/" />
     }
 
     return (
     <View className="flex-1">
         <Image source={product.cover} className="w-full h-52" resizeMode="cover"/>
 
+
         <View className="p-5 mt-8 flex-1">
+            
+            <Text className="text-white text-xl font-heading">
+                {product.title}
+            </Text>
+
             <Text className=" text-lime-400 text-2xl font-heading my-2"> 
                 {formatCurrency(product.price)}
             </Text>
